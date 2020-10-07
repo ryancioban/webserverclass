593c63dd903d4b244b9379bcf5d35d6b1de5737f
ryancioban <70660684+ryancioban@users.noreply.github.com>
2020-10-06 20:16:41 -0400

    Test forms.php
    PHP form validation + required fields testing.

--- forms.php
+++ forms.php
@@ -13,23 +13,61 @@
         <div>Tell me about yourself below:</div>
         <br/>
 
-        <form action="submitted.php" method="post">
-            Name: <input type="text" id="name" name="name"><br/><br/>
+        <!--PHP form validation-->
+        <?php
+        //Define variables and set to empty values
+        $nameErr = $ageErr = $genderErr = "";
+        $name = $age =  $gender = "";
 
-            Age: <input type="text" id="age" name="age"><br/><br/>
+        if ($_SERVER["REQUEST_METHOD"] == "POST") {
+            if (empty($_POST["name"])) {
+                $nameErr = "Name is required.";
+            } else {
+                $name = test_input($_POST["name"]);
+            }
+            
+            if (empty($_POST["age"])) {
+                $ageErr = "Age is required.";
+            } else {
+                $age = test_input($_POST["age"]);
+            }
+
+            if (empty($_POST["gender"])) {
+               $genderErr = "Choice is required."; 
+            } else {
+                $gender = test_input($_POST["gender"]);
+            }
+        }
+
+        function test_input($data) {
+            $data = trim($data);
+            $data = stripslashes($data);
+            $data = htmlspecialchars($data);
+            return $data;
+        }
+        ?>
+
+        <!--Form elements-->
+        <p><span class="error">* required field</span></p>
+        <form action="submitted.php" method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
+            Name: <input type="text" id="name" name="name">
+            <span class="error">* <?= $nameErr;?></span> <br/><br/>
+
+            Age: <input type="text" id="age" name="age">
+            <span class="error">* <?= $ageErr;?></span> <br/><br/>
             
             <div>Gender:</div>
             <input type="radio" id="gender" name="gender" value="Male"> Male <br/>
             <input type="radio" id="gender" name="gender" value="Female"> Female <br/>
-            <input type="radio" id="gender" name="gender" value="Other"> Other <br/>
-            <br/>
+            <input type="radio" id="gender" name="gender" value="n/a"> Other 
+            <span class="error">* <?= $genderErr;?></span> <br/><br/>
 
             Preferred drink: <select id="drink" name="drink">
                 <option value="Alcohol">Alcoholic</option>
                 <option value="Non-Alcoholic">Non-Alcoholic</option>
                 <option value="Water">Water</option>
-            </select><br/>
-            <input type="submit">
+            </select><br/><br/>
+            <input type="submit" name="submit" value="Submit">
         </form>
         </br>
 
