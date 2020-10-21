a3d9a4f8e8dce8f99926ea7c17ad0373d0085131
ryancioban <70660684+ryancioban@users.noreply.github.com>
2020-10-21 16:14:35 -0400

    Update data.php

--- data.php
+++ data.php
@@ -24,7 +24,7 @@
             if (mysqli_num_rows($result) > 0) {
                 //output data of each row
                 while($row = mysqli_fetch_assoc($result)) {
-                    echo "Entryid: " . $row["Entryid"]. " Name: " . $row["Name"]. " Gender: " .$row["Gender"]. " Drink: " . $row["Drinktype"]. "<br>";
+                    echo "Entry #: " . $row["Entryid"]. " Name: " . $row["Name"]. "<br>" "Gender: " .$row["Gender"]. "<br>" "Drink: " . $row["Drinktype"]. "<br>";
                 }
             } else {
                 echo "0 results";
