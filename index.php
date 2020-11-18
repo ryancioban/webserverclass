336635820748d19b2b0b48ccd2d86a33398ff5b0
ryancioban <70660684+ryancioban@users.noreply.github.com>
2020-11-18 01:37:42 -0500

    testing auto-refresh

--- index.php
+++ index.php
@@ -1,9 +1,15 @@
 <!DOCTYPE html>
+<?php
+    $page = $_SERVER['PHP_SELF'];
+    $sec = "10";
+?>
 <html>
      <head>
         <title>Drinking Time</title>
         <!--Change picture on index page by hovering over it with the mouse-->
         <script src="swap.js"></script>
+        <!--Auto-refresh page-->
+        <meta http-equiv="refresh" content="<?php echo $sec?>;URL='<?php echo $page?>'">
     </head>
     
     <body>
